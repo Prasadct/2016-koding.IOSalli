4709e667975045d02c621c708502cb6df83913f7
prasadct <prasadct99@gmail.com>
2016-02-20 23:19:12 +0530

    factories

---
+++ factories/factories.js
@@ -0,0 +1,29 @@
+var app = angular.module('myFactories',['mongolabResourceHttp'])
+
+
+
+
+app.factory('messages',function(){
+    
+  var messages = {};
+
+  messages.list = [];
+
+  messages.add = function(message){
+    messages.list.push({id: messages.list.length, text: message});
+  };
+
+  return messages;
+});
+
+app.factory('Hospital', function ($mongolabResourceHttp) {
+    return $mongolabResourceHttp('hospitals');
+})
+
+app.factory('Medication', function ($mongolabResourceHttp) {
+    return $mongolabResourceHttp('medications');
+})
+
+app.factory('Dealer', function ($mongolabResourceHttp) {
+    return $mongolabResourceHttp('dealers');
+})
